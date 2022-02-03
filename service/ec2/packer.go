d39a345fe2fff88508ed1922f2f50307e9db9bd9
Jake Rawsthorne <jake.rawsthorne@couchbase.com>
2022-02-03 11:59:08 +0000

    Support centos7 on ec2
    Older server versions don't supply an amzn2 package

--- service/ec2/packer.go
+++ service/ec2/packer.go
@@ -21,13 +21,36 @@ func addArg(args *[]string, arg string) {
 	*args = append(*args, arg)
 }
 
+type AMIArg struct {
+	SourceAMIFilter string
+	Owner           string
+}
+
 // os -> arch -> ami
-var packageToSourceAMIFilter = map[string]map[string]string{
-	"amzn2": {"aarch64": "amzn2-ami-hvm-2.0.*.1-arm64-gp2", "x86_64": "amzn2-ami-hvm-2.0.*.1-x86_64-gp2"},
+var packageToAMIArg = map[string]map[string]AMIArg{
+	"amzn2":   {"aarch64": AMIArg{SourceAMIFilter: "amzn2-ami-hvm-2.0.*.1-arm64-gp2", Owner: "amazon"}, "x86_64": AMIArg{SourceAMIFilter: "amzn2-ami-hvm-2.0.*.1-x86_64-gp2", Owner: "amazon"}},
+	"centos7": {"x86_64": AMIArg{SourceAMIFilter: "CentOS Linux 7 x86_64 HVM EBS *", Owner: "679593333241"}},
+}
+
+var osToSSHUsername = map[string]string{
+	"amzn2":   "ec2-user",
+	"centos7": "centos",
+}
+
+var osToPackerfilePrefix = map[string]string{
+	"amzn2":   "yum",
+	"centos7": "yum",
+}
+
+var osToDeviceName = map[string]string{
+	"amzn2":   "/dev/xvda",
+	"centos7": "/dev/sda1",
 }
 
 func CallPacker(opts PackerOptions) error {
-	err := exec.Command("packer", "init", "packerfiles").Run()
+	filePath := fmt.Sprintf("packerfiles/aws-%s.pkr.hcl", osToPackerfilePrefix[opts.OS])
+
+	err := exec.Command("packer", "init", filePath).Run()
 
 	if err != nil {
 		return err
@@ -42,8 +65,11 @@ func CallPacker(opts PackerOptions) error {
 	addArg(&args, "base_url="+opts.BaseUrl)
 	addArg(&args, "ami_name="+opts.AmiName)
 	addArg(&args, "arch="+opts.Arch)
-	addArg(&args, "source_ami_filter="+packageToSourceAMIFilter[opts.OS][opts.Arch])
-	args = append(args, fmt.Sprintf("packerfiles/aws-%s.pkr.hcl", opts.OS))
+	addArg(&args, "source_ami_filter="+packageToAMIArg[opts.OS][opts.Arch].SourceAMIFilter)
+	addArg(&args, "ssh_username="+osToSSHUsername[opts.OS])
+	addArg(&args, "ami_owner="+packageToAMIArg[opts.OS][opts.Arch].Owner)
+	addArg(&args, "device_name="+osToDeviceName[opts.OS])
+	args = append(args, filePath)
 
 	cmd := exec.Command("packer", args...)
 
