f0e8ac59b65bfa556c3d12fd71b42a3c3acb7c83
Jake Rawsthorne <jake.rawsthorne@couchbase.com>
2022-05-03 09:54:08 +0100

    Add support for morpheus

--- service/common/nodes.go
+++ service/common/nodes.go
@@ -95,7 +95,7 @@ var versionToFlavor = map[int]map[int]string{
 	4: {0: "sherlock", 5: "watson"},
 	5: {0: "spock", 5: "vulcan"},
 	6: {0: "alice", 5: "mad-hatter"},
-	7: {0: "cheshire-cat", 1: "neo"},
+	7: {0: "cheshire-cat", 1: "neo", 2: "morpheus"},
 }
 
 func flavorFromVersion(version string) (string, error) {
